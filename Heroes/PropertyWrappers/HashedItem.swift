72502f35e410561b3e05b20ff0293131da50213c
Marcos Curvello <mrcurvello@gmail.com>
2020-06-05 03:51:51 -0300

    Refactor method declarations

--- Heroes/PropertyWrappers/HashedItem.swift
+++ Heroes/PropertyWrappers/HashedItem.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+import CryptoKit
+
+@propertyWrapper
+public struct HashedItem<Value: StringProtocol> {
+    private var storage: Value?
+    
+    public init() { storage = nil }
+    
+    public var wrappedValue: Value? {
+        get {
+            guard let value = storage else {
+                return nil
+            }
+            return value
+        }
+        set {
+            if let v = newValue {
+                storage = hashedValue(v)
+            } else {
+                storage = nil
+            }
+        }
+    }
+    
+    private func hashedValue(_ value: Value) -> Value {
+        let data = value.data(using: .utf8)!
+        let hashedValue = hash(data: data)
+        return hashedValue as! Value
+    }
+    
+    private func hash(data: Data) -> String {
+        let digest = Insecure.MD5.hash(data: data)
+        let hashedString = digest.map { String(format: "%02hhx", $0) }.joined()
+        return hashedString
+    }
+    
+}
