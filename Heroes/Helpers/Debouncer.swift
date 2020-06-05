193481c3085ba28ab5481f11ce8c077b1f526ded
Marcos Curvello <mrcurvello@gmail.com>
2020-06-05 20:49:02 -0300

    Feature collection view search results and loader reusable views

---
+++ Heroes/Helpers/Debouncer.swift
@@ -0,0 +1,34 @@
+//
+//  Debouncer.swift
+//  Heroes
+//
+//  Created by Marcos Curvello on 05/06/20.
+//  Copyright © 2020 Marcos Curvello. All rights reserved.
+//
+
+import Foundation
+
+class Debouncer {
+    private var workItem: DispatchWorkItem = DispatchWorkItem(block: {})
+    private var previousRun: Date = Date.distantPast
+    private let queue: DispatchQueue
+    private let minimumDelay: TimeInterval
+
+    init(minimumDelay: TimeInterval, queue: DispatchQueue = DispatchQueue.main) {
+        self.minimumDelay = minimumDelay
+        self.queue = queue
+    }
+
+    func debounce(_ block: @escaping () -> Void) {
+        workItem.cancel()
+
+        workItem = DispatchWorkItem() { [weak self] in
+            self?.previousRun = Date()
+            block()
+        }
+
+        let delay = previousRun.timeIntervalSinceNow > minimumDelay ? 0 : minimumDelay
+        queue.asyncAfter(deadline: .now() + Double(delay), execute: workItem)
+    }
+    
+}
