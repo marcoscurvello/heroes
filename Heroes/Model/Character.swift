63ddc97bef6e2c012dcdc90a08b78c9af03fa831
Marcos Curvello <mrcurvello@gmail.com>
2020-06-05 03:50:05 -0300

    Feature model objects

---
+++ Heroes/Model/Character.swift
@@ -0,0 +1,34 @@
+//
+//  Character.swift
+//  Heroes
+//
+//  Created by Marcos Curvello on 05/06/20.
+//  Copyright © 2020 Marcos Curvello. All rights reserved.
+//
+
+import Foundation
+
+struct Character: Decodable {
+    let id: Int
+    let name: String?
+    let description: String?
+    let modified: String?
+    let resourceURI: String?
+    let urls: [Url]?
+    let thumbnail: Image?
+    let comics: ResourceList?
+    let stories: ResourceList?
+    let events: ResourceList?
+    let series: ResourceList?
+}
+
+extension Character: Hashable, Equatable {
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+        hasher.combine(name)
+    }
+    
+    static func == (lhs: Character, rhs: Character) -> Bool {
+        return lhs.id == rhs.id && lhs.name == rhs.name
+    }
+}
