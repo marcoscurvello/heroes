66b4f0e0b1ac33be8057277d7099df4824ba574e
Marcos Curvello <mrcurvello@gmail.com>
2020-06-09 12:29:32 -0300

    Feature view controller to list character relational objects

---
+++ Heroes/View/HeroDetail/Cells/ComicCell.swift
@@ -0,0 +1,86 @@
+//
+//  ComicCell.swift
+//  Heroes
+//
+//  Created by Marcos Curvello on 06/06/20.
+//  Copyright © 2020 Marcos Curvello. All rights reserved.
+//
+
+import UIKit
+
+class ComicCell: UICollectionViewCell {
+    
+    static let reuseIdentifier = "comic-cell-reuse-identifier"
+    var representedIdentifier: String?
+    
+    let imageView = UIImageView()
+    let titleLabel = UILabel()
+    let categoryLabel = UILabel()
+    
+    required init?(coder: NSCoder) { super.init(coder: coder) }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.imageView.image = nil
+        self.titleLabel.text = nil
+        self.categoryLabel.text = nil
+    }
+    
+    func display(image: UIImage?) {
+        DispatchQueue.main.async {
+            guard let image = image else {
+                self.imageView.contentMode = .center
+                self.imageView.backgroundColor = .systemGray6
+                self.imageView.image = placeholderImage
+                return
+            }
+            self.imageView.contentMode = .scaleToFill
+            self.imageView.image = image
+        }
+    }
+    
+}
+
+extension ComicCell {
+    
+    func configure() {
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        categoryLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        contentView.addSubview(imageView)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(categoryLabel)
+        
+        titleLabel.font = UIFont.preferredFont(forTextStyle: .caption1)
+        titleLabel.adjustsFontForContentSizeCategory = true
+        categoryLabel.font = UIFont.preferredFont(forTextStyle: .caption2)
+        categoryLabel.adjustsFontForContentSizeCategory = true
+        categoryLabel.textColor = .placeholderText
+        
+        imageView.layer.borderColor = UIColor.black.cgColor
+        imageView.layer.borderWidth = 1
+        imageView.layer.cornerRadius = 4
+                
+        let spacing = CGFloat(10)
+        NSLayoutConstraint.activate([
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            
+            titleLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: spacing),
+            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            
+            categoryLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor),
+            categoryLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            categoryLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            categoryLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+    }
+    
+}
