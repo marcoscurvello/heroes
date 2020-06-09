ba9b98d0a5aee23261c2681deefd9e5156812839
Marcos Curvello <mrcurvello@gmail.com>
2020-06-09 10:34:33 -0300

    Refactor to init base view controller programatically

--- Heroes/SceneDelegate.swift
+++ Heroes/SceneDelegate.swift
@@ -17,7 +17,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let charactersCollectionView = HeroListViewController(layout: CollectionViewLayoutGenerator.generateLayoutForStyle(.paginated))
+        
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = UINavigationController(rootViewController: charactersCollectionView)
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -48,7 +55,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
 
         // Save changes in the application's managed object context when the application transitions to the background.
-        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+        CoredataStack.shared.saveContext()
     }
 
 
