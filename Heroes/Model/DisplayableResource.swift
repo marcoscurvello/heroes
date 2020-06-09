24ffa87acbbacee1b14cc442418ec6f00079c326
Marcos Curvello <mrcurvello@gmail.com>
2020-06-09 10:37:12 -0300

    Feature displayable resource type

---
+++ Heroes/Model/DisplayableResource.swift
@@ -0,0 +1,28 @@
+//
+//  DisplayableResource.swift
+//  Heroes
+//
+//  Created by Marcos Curvello on 07/06/20.
+//  Copyright © 2020 Marcos Curvello. All rights reserved.
+//
+
+import Foundation
+
+protocol Displayable {
+    func convert(type: Self) -> DisplayableResource
+}
+
+struct DisplayableResource {
+    enum ResourceType { case comic, event, serie, story }
+    let type: ResourceType
+    let id: Int
+    let title: String
+    let description: String?
+    let thumbnail: Image?
+}
+
+extension DisplayableResource: Hashable {
+    static func == (lhs: DisplayableResource, rhs: DisplayableResource) -> Bool {
+        lhs.id == rhs.id && lhs.title == rhs.title
+    }
+}
