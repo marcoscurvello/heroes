8b33bb9fbd14b45f0eccb7705bc970f17be14578
Marcos Curvello <mrcurvello@gmail.com>
2020-06-18 11:59:39 -0300

    Feature resource data source

--- Heroes/View/HeroList/HeroDataSource.swift
+++ Heroes/View/HeroList/HeroDataSource.swift
@@ -9,7 +9,34 @@
 import UIKit
 
 typealias HeroSnapshot = NSDiffableDataSourceSnapshot<HeroDataSource.Section, Character>
-
 class HeroDataSource: UICollectionViewDiffableDataSource<HeroDataSource.Section, Character> {
     enum Section { case main }
+}
+
+typealias ResourceSnapshot = NSDiffableDataSourceSnapshot<ResourceDataSource.LayoutSection, DisplayableResource>
+class ResourceDataSource: UICollectionViewDiffableDataSource<ResourceDataSource.LayoutSection, DisplayableResource> {
+    
+    enum LayoutSection: Int, CaseIterable {
+        case comics, stories, events, series
+        
+        var sectionTitle: String {
+            switch self {
+            case .comics: return "Comics"
+            case .stories: return "Stories"
+            case .events: return "Events"
+            case .series: return "Series"
+            }
+        }
+        
+        func columnCount(for width: CGFloat) -> Int {
+            let wideMode = width > 800
+            let narrowMode = width < 420
+            
+            switch self {
+            case .comics, .stories, .events, .series:
+                return wideMode ? 3 : narrowMode ? 1 : 2
+            }
+        }
+    }
+    
 }
