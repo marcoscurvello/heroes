b3ab15070b20089c9da0f6b79bfb89380acd1907
Marcos Curvello <mrcurvello@gmail.com>
2020-06-18 08:56:04 -0300

    Refactor implement decoding method to provide default values

--- Heroes/Model/Character.swift
+++ Heroes/Model/Character.swift
@@ -7,11 +7,17 @@
 //
 
 import Foundation
+import CoreData
 
-struct Character: Decodable {
+protocol Persistable {
+    associatedtype ManagedObject: NSManagedObject
+    init(managedObject: ManagedObject)
+}
+
+struct Character {
     let id: Int
     let name: String
-    let description: String?
+    let description: String
     let modified: String?
     let resourceURI: String?
     let urls: [Url]?
@@ -22,6 +28,41 @@ struct Character: Decodable {
     let series: ResourceList?
 }
 
+extension Character: Decodable {
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case name = "name"
+        case description = "description"
+        case modified = "modified"
+        case resourceURI = "resourceURI"
+        case urls = "urls"
+        case thumbnail = "thumbnail"
+        case comics = "comics"
+        case stories = "stories"
+        case events = "events"
+        case series = "series"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        id = try values.decode(Int.self, forKey: .id)
+        name = try values.decode(String.self, forKey: .name)
+        let dsc = try values.decodeIfPresent(String.self, forKey: .description)!
+        description = dsc.isEmpty ? "No description availiable" : dsc
+        modified = try values.decode(String.self, forKey: .modified)
+        resourceURI = try values.decode(String.self, forKey: .resourceURI)
+        urls = try values.decode([Url].self, forKey: .urls)
+        thumbnail = try values.decode(Image.self, forKey: .thumbnail)
+        comics = try values.decode(ResourceList.self, forKey: .comics)
+        stories = try values.decode(ResourceList.self, forKey: .stories)
+        events = try values.decode(ResourceList.self, forKey: .events)
+        series = try values.decode(ResourceList.self, forKey: .series)
+    }
+    
+}
+
+
 extension Character: Hashable, Equatable {
     func hash(into hasher: inout Hasher) {
         hasher.combine(id)
@@ -29,5 +70,23 @@ extension Character: Hashable, Equatable {
     }
     static func == (lhs: Character, rhs: Character) -> Bool {
         return lhs.id == rhs.id && lhs.name == rhs.name
+    }
+}
+
+extension Character: Persistable {
+    typealias ManagedObject = CharacterObject
+    
+    init(managedObject: CharacterObject) {
+        id = Int(managedObject.id)
+        name = managedObject.name
+        description = managedObject.descriptionText
+        modified = nil
+        resourceURI = managedObject.resourceURI
+        urls = nil
+        thumbnail = Image(path: managedObject.image?.path, extension: managedObject.image?.type, data: managedObject.image?.data)
+        comics = nil
+        stories = nil
+        events = nil
+        series = nil
     }
 }
