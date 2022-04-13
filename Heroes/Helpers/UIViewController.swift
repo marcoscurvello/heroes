366926be9d7ee23a97a9a493562cbc41592ea38d
Marcos Curvello <mrcurvello@gmail.com>
2022-04-13 13:03:23 +0100

    Fix remove duplicate dispatch async call

--- Heroes/Helpers/UIViewController.swift
+++ Heroes/Helpers/UIViewController.swift
@@ -11,21 +11,17 @@ import UIKit
 extension UIViewController {
 
     func presentAlertWithError(message: UserFriendlyError, callback: @escaping (Bool) -> Void) {
-        DispatchQueue.main.async {
-            let alert = UIAlertController(title: message.title, message: message.message, preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "Report", style: .default, handler: { _ in callback(true) }))
-            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in callback(false) }))
-            self.present(alert, animated: true)
-        }
+        let alert = UIAlertController(title: message.title, message: message.message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Report", style: .default, handler: { _ in callback(true) }))
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in callback(false) }))
+        DispatchQueue.main.async { self.present(alert, animated: true) }
     }
 
     func presentAlertWithStateChange(message: StateChangeMessage, callback: @escaping (Bool) -> ()) {
-        DispatchQueue.main.async {
-            let alert = UIAlertController(title: message.title, message: message.message, preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "Delete", style: .destructive) { _ in callback(true) })
-            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { _ in callback(false) }))
-            DispatchQueue.main.async { self.present(alert, animated: true) }
-        }
+        let alert = UIAlertController(title: message.title, message: message.message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Delete", style: .destructive) { _ in callback(true) })
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { _ in callback(false) }))
+        DispatchQueue.main.async { self.present(alert, animated: true) }
     }
 
 }
